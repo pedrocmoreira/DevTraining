1dae1cdafa0eb8a648e459fc96316360427eb7cc
Pedro Moreira <pedrocmoreira97@gmail.com>
2023-12-26 20:00:19 -0300

    criando a tabela tags através de migração

--- src/database/orm-cli-config.ts
+++ src/database/orm-cli-config.ts
@@ -3,12 +3,14 @@ import { DataSource } from 'typeorm';
 import { dataSourceOptions } from './database.module';
 
 import { CreateCoursesTable1703630245950 } from 'src/migrations/1703630245950-CreateCoursesTable';
+import { CreateTagsTable1703631456765 } from 'src/migrations/1703631456765-CreateTagsTable';
 
 export const dataSource = new DataSource({
   ...dataSourceOptions,
   synchronize: false,
   migrations: [
     CreateCoursesTable1703630245950,
+    CreateTagsTable1703631456765
   ]
 });
 
--- src/migrations/1703631456765-CreateTagsTable.ts
+++ src/migrations/1703631456765-CreateTagsTable.ts
@@ -1,11 +1,10 @@
 import { MigrationInterface, QueryRunner, Table } from "typeorm"
 
-export class CreateCoursesTable1703630245950 implements MigrationInterface {
+export class CreateTagsTable1703631456765 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"')
         await queryRunner.createTable(new Table({
-            name: 'courses',
+            name: 'tags',
             columns: [
                 {
                     name: 'id',
@@ -17,20 +16,16 @@ export class CreateCoursesTable1703630245950 implements MigrationInterface {
                     type: 'varchar'
                 },
                 {
-                    name: 'description',
-                    type: 'varchar'
-                },
-                {
                     name: 'created_at',
                     type: 'timestamp',
                     default: 'CURRENT_TIMESTAMP',
                 },
             ]
-        }))
+        }));
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.dropTable('courses');
+        await queryRunner.dropTable('tags')
     }
 
 }
