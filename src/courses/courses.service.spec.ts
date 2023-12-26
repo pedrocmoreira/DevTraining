30371101e743947ed3420d2cc91c125838e65feb
Pedro Moreira <pedrocmoreira97@gmail.com>
2023-12-26 20:47:42 -0300

    configurando testes unitários de CoursesService

--- src/courses/courses.service.spec.ts
+++ src/courses/courses.service.spec.ts
@@ -1,15 +1,48 @@
-import { Test, TestingModule } from '@nestjs/testing';
+import { randomUUID } from 'node:crypto';
 import { CoursesService } from './courses.service';
 
-describe('CoursesService', () => {
+describe('CoursesService unit tests', () => {
   let service: CoursesService;
+  let id: string
+  let created_at: Date;
+  let expectOutputTags: any;
+  let expectOutputCourses: any;
+  let mockCourseRepository: any;
+  let mockTagRepository: any;
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [CoursesService],
-    }).compile();
-
-    service = module.get<CoursesService>(CoursesService);
+    service = new CoursesService();
+    id = randomUUID();
+    created_at = new Date();
+    expectOutputTags = [
+      {
+        id,
+        name: 'nestjs',
+        created_at
+      }
+    ];
+    expectOutputCourses =
+    {
+      id,
+      name: 'test',
+      description: 'test description',
+      created_at,
+      tags: expectOutputTags,
+    };
+    mockCourseRepository = {
+      create: jest.fn().mockReturnValue(Promise.resolve(expectOutputCourses)),
+      save: jest.fn().mockReturnValue(Promise.resolve(expectOutputCourses)),
+      update: jest.fn().mockReturnValue(Promise.resolve(expectOutputCourses)),
+      preload: jest.fn().mockReturnValue(Promise.resolve(expectOutputCourses)),
+      findAll: jest.fn().mockReturnValue(Promise.resolve(expectOutputCourses)),
+      find: jest.fn().mockReturnValue(Promise.resolve(expectOutputCourses)),
+      findOne: jest.fn().mockReturnValue(Promise.resolve(expectOutputCourses)),
+      remove: jest.fn().mockReturnValue(Promise.resolve(expectOutputCourses)),
+    };
+    mockTagRepository = {
+      create: jest.fn().mockReturnValue(Promise.resolve(expectOutputTags)),
+      findOne: jest.fn(),
+    }
   });
 
   it('should be defined', () => {
