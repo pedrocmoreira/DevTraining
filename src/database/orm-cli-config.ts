31b798bc8757c60510045cab86083d06e65c60b5
Pedro Moreira <pedrocmoreira97@gmail.com>
2023-12-26 20:29:21 -0300

    criando a coluna tagId na tabela courses_tags através de migração

--- src/database/orm-cli-config.ts
+++ src/database/orm-cli-config.ts
@@ -6,6 +6,7 @@ import { CreateCoursesTable1703630245950 } from 'src/migrations/1703630245950-Cr
 import { CreateTagsTable1703631456765 } from 'src/migrations/1703631456765-CreateTagsTable';
 import { CreateCoursesTagsTable1703632028176 } from 'src/migrations/1703632028176-CreateCoursesTagsTable';
 import { AddCoursesIdToCoursesTags1703632309728 } from 'src/migrations/1703632309728-AddCoursesIdToCoursesTags';
+import { AddTagsIdToCoursesTagsTable1703632764169 } from 'src/migrations/1703632764169-AddTagsIdToCoursesTagsTable';
 
 export const dataSource = new DataSource({
   ...dataSourceOptions,
@@ -14,7 +15,8 @@ export const dataSource = new DataSource({
     CreateCoursesTable1703630245950,
     CreateTagsTable1703631456765,
     CreateCoursesTagsTable1703632028176,
-    AddCoursesIdToCoursesTags1703632309728
+    AddCoursesIdToCoursesTags1703632309728,
+    AddTagsIdToCoursesTagsTable1703632764169
   ]
 });
 
