d4c36a827e2bb3ecf6bddc3f9cbd6abfdc844a2c
Pedro Moreira <pedrocmoreira97@gmail.com>
2023-12-26 20:11:17 -0300

    criando migration para a tabela courses_tags

--- src/database/orm-cli-config.ts
+++ src/database/orm-cli-config.ts
@@ -4,13 +4,15 @@ import { dataSourceOptions } from './database.module';
 
 import { CreateCoursesTable1703630245950 } from 'src/migrations/1703630245950-CreateCoursesTable';
 import { CreateTagsTable1703631456765 } from 'src/migrations/1703631456765-CreateTagsTable';
+import { CreateCoursesTagsTable1703632028176 } from 'src/migrations/1703632028176-CreateCoursesTagsTable';
 
 export const dataSource = new DataSource({
   ...dataSourceOptions,
   synchronize: false,
   migrations: [
     CreateCoursesTable1703630245950,
-    CreateTagsTable1703631456765
+    CreateTagsTable1703631456765,
+    CreateCoursesTagsTable1703632028176,
   ]
 });
 
