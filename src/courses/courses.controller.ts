863c78d68f9f920a3d8ca218ee26ba8a8ed1cce9
Pedro Moreira <pedrocmoreira97@gmail.com>
2023-12-26 14:09:06 -0300

    criando DTO's

--- src/courses/courses.controller.ts
+++ src/courses/courses.controller.ts
@@ -1,5 +1,7 @@
 import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, Post, Put } from '@nestjs/common';
 import { CoursesService } from './courses.service';
+import { CreateCourseDTO } from './dto/create-course.dto';
+import { UpdateCourseDTO } from './dto/update-course.dto';
 
 @Controller('courses')
 export class CoursesController {
@@ -17,15 +19,15 @@ export class CoursesController {
   }
 
   @Post()
-  create(@Body() body) {
-    return this.courseService.create(body);
+  create(@Body() createCourseDTO: CreateCourseDTO) {
+    return this.courseService.create(createCourseDTO);
   }
 
   //Put para atualizar mais de uma informação Patch para atualizar apenas uma informação
 
   @Put()
-  update(@Param('id') id: number, @Body() body) {
-    return this.courseService.update(+id, body);
+  update(@Param('id') id: number, @Body() updateCourseDTO: UpdateCourseDTO) {
+    return this.courseService.update(+id, updateCourseDTO);
   }
 
   @HttpCode(HttpStatus.NO_CONTENT)
